e20a9d8084c22969b5665993f1e9f4427ef43470
Lovepreet Kumar <96973220+lovepreet-k828@users.noreply.github.com>
2023-05-11 18:17:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ async def get_net_image_prediction(data=Body()):
     if(nlocation!=""):
         nlocation = "\nNear by location: "+nlocation
     if(glocation!=""):
-        glocation = "\nLocation on Google maps: "+nlocation
+        glocation = "\nLocation on Google maps: "+glocation
     if footage_video == "":
         return {"message": "No video provided"}
     if email_id == "":
@@ -136,7 +136,7 @@ async def get_net_image_prediction(data=Body()):
             acc_times.append(acc_time)
             ids.append(id)
             subject = "Accident Detected at India time:"+ acc_time 
-            text ="Accident Details \nMode: Recorded Video\nDate & Time: "+datetime_NY.strftime("%Y-%m-%d %H:%M:%S.%f")+"\nClip link: " +video_link +nlocation+glocation+"\n Clip Id: "+id
+            text ="Accident Details \nMode: Recorded Video\nDate & Time: "+datetime_NY.strftime("%Y-%m-%d %H:%M:%S.%f")+"\nClip link: " +video_link +nlocation+glocation+"\nClip Id: "+id
             message = """From: %s\nTo: %s\nSubject: %s\n\n%s
             """ % (sender, ", ".join(receivers), subject, text)
             s.sendmail(sender, receivers, message)
